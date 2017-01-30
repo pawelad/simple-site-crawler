59a6084f34e77809d1964792e31edccdd238aec7
Paweł Adamczak <pawel.ad@gmail.com>
2017-01-30 21:01:30 +0000

    Added more project meta info

--- simple_site_crawler/__init__.py
+++ simple_site_crawler/__init__.py
@@ -1,4 +1,13 @@
+"""
+simple-site-crawler - Simple website crawler that asynchronously crawls a
+    website and all subpages that it could find, along with static content
+    that they rely on.
+"""
 from simple_site_crawler.site_crawler import SiteCrawler  # noqa
 
 
+__title__ = 'simple-site-crawler'
 __version__ = '0.1.0'
+__author__ = 'Paweł Adamczak'
+__license__ = 'MIT'
+__url__ = 'https://github.com/pawelad/simple-site-crawler'
